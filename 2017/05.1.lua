5484a46f8492e50d61d4a29ebb28dcc6d1d746f1
Henri Derycke <henri.derycke@ens-lyon.org>
2017-12-05 09:49:53 +0100

    Day 5

---
+++ 2017/05.1.lua
@@ -0,0 +1,12 @@
+function f(s)
+  local t = load('return {'..s:gsub('-?%d+','%1,')..'}')() 
+  local ret = 0
+  local i = 1
+  while i <= #t do
+    i,t[i] = i+t[i], t[i]+1
+    ret = ret + 1 
+  end
+  return ret
+end
+
+print(f(io.read("a")))
