070ec2791605b82bb95ca178824c1feb6d96f761
nheir <henri.derycke@ens-lyon.org>
2017-12-13 21:27:04 +0100

    Split day 02

---
+++ 2017/02.2.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua
+
+local res = 0
+for s in io.lines() do
+	local t = {}
+	for d in s:gmatch('%d+') do
+		table.insert(t, tonumber(d))
+	end
+	if #t == 0 then break end
+	local b = false
+	for i=1,#t do for j=i+1,#t do
+		if t[i] % t[j] == 0 then
+			res = res + t[i] / t[j]
+			b = true
+		end
+		if t[j] % t[i] == 0 then
+			res = res + t[j] / t[i]
+			b = true
+		end
+		if b then break end
+	end if b then break end end
+end
+print(res)
