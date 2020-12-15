4b930da288adcb5b59e6eefbd09977ea296d5ff0
Henri Derycke <henri.derycke@ens-lyon.org>
2020-12-15 10:16:06 +0100

    Day 15

---
+++ 2020/15.1.lua
@@ -0,0 +1,23 @@
+local t = {}
+local nt = {}
+local seq = {}
+
+local i = 1
+local last
+for n in io.read('a'):gmatch('%d+') do
+	n = tonumber(n)
+	last = n
+	t[n],nt[n] = nt[n],i
+	i = i + 1
+end
+
+for j=i, 2020 do
+	if t[last] then
+		last = nt[last] - t[last]
+	else
+		last = 0
+	end
+	t[last],nt[last] = nt[last],j
+end
+
+print(last)
