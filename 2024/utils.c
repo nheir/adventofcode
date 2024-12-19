b974cc583760a5590345403932473bf2326f83cc
Henri Derycke <henri.derycke@ens-lyon.org>
2024-12-19 12:57:17 +0100

    update utils

--- 2024/utils.c
+++ 2024/utils.c
@@ -6,24 +6,23 @@
 #include "utils.h"
 
 struct str read_input() {
-    struct str str = {.data = malloc(BUFSIZ), BUFSIZ, 0};
+    DA_NEW(str, unsigned char);
     unsigned char buf[BUFSIZ];
     size_t len;
     while ((len = fread(buf, 1, BUFSIZ, stdin)) > 0) {
-        if (len + str.len >= str.capacity) {
-            str.capacity *= 2;
-            unsigned char *ptr = realloc(str.data, str.capacity);
-            if (ptr == NULL) {
-                perror("not enough memory...");
-                free(str.data);
-                exit(EXIT_FAILURE);
-            }
-            str.data = ptr;
-        }
-        memcpy(str.data + str.len, buf, len);
-        str.len += len;
+        DA_EXTEND(str, unsigned char, buf, len);
     }
-    return str;
+    return (struct str){.data = str.data, .len = str.len, .capacity = str.capacity};
+}
+
+struct sv sv_read_input() {
+    DA_NEW(str, unsigned char);
+    unsigned char buf[BUFSIZ];
+    size_t len;
+    while ((len = fread(buf, 1, BUFSIZ, stdin)) > 0) {
+        DA_EXTEND(str, unsigned char, buf, len);
+    }
+    return (struct sv){.data = str.data, .len = str.len};
 }
 
 void str_destroy(struct str *str) {
@@ -46,9 +45,38 @@ struct pair split(struct sv sv, char byte) {
     return (struct pair){sv, {0}};
 }
 
+struct pair sv_split_byte(struct sv sv, char byte) {
+    return split(sv, byte);
+}
+
+struct pair sv_split_once(struct sv sv, struct sv sep) {
+    for (size_t i = 0; i < sv.len; i++) {
+        if (sv_startswith((struct sv){.data = sv.data + i, .len = sv.len - i}, sep)) {
+            return (struct pair){
+                .left = {.data = sv.data, .len = i},
+                .right = {.data = sv.data + i + sep.len, .len = sv.len - i - sep.len},
+            };
+        }
+    }
+    return (struct pair){sv, {0}};
+}
+
 long sv_read_int(struct sv sv) {
     long ret = 0;
     for (size_t i = 0; i < sv.len && '0' <= sv.data[i] && sv.data[i] <= '9'; i++)
         ret = ret * 10 + (sv.data[i] - '0');
     return ret;
+}
+
+int sv_startswith(struct sv sv, struct sv pref) {
+    return sv.len >= pref.len && memcmp(sv.data, pref.data, pref.len) == 0;
+}
+
+struct sv *sv_split_array(struct sv sv, struct sv sep, size_t *n) {
+    DA_NEW(da, struct sv);
+    for (struct pair p = sv_split_once(sv, sep); p.left.data; p = sv_split_once(p.right, sep)) {
+        DA_APPEND(da, struct sv, p.left);
+    }
+    *n = da.len;
+    return da.data;
 }
--- 2024/utils.h
+++ 2024/utils.h
@@ -3,6 +3,61 @@
 
 #include <stddef.h>
 
+#define DA_NEW(name, T)                                                                                                \
+    struct {                                                                                                           \
+        T *data;                                                                                                       \
+        size_t len;                                                                                                    \
+        size_t capacity;                                                                                               \
+    } name = {0};
+
+#define DA_APPEND(name, T, value)                                                                                      \
+    do {                                                                                                               \
+        if (name.len >= name.capacity) {                                                                               \
+            if (!name.capacity)                                                                                        \
+                name.capacity = 256;                                                                                   \
+            else                                                                                                       \
+                name.capacity *= 2;                                                                                    \
+            void *ptr = realloc(name.data, sizeof(T) * name.capacity);                                                 \
+            if (ptr == NULL) {                                                                                         \
+                perror("not enough memory...");                                                                        \
+                free(name.data);                                                                                       \
+                exit(EXIT_FAILURE);                                                                                    \
+            }                                                                                                          \
+            name.data = ptr;                                                                                           \
+        }                                                                                                              \
+        memcpy(name.data + name.len, &value, sizeof(T));                                                               \
+        name.len++;                                                                                                    \
+    } while (0);
+
+#define DA_EXTEND(name, T, value, size)                                                                                \
+    do {                                                                                                               \
+        if (name.len + (size) > name.capacity) {                                                                       \
+            if (!name.capacity)                                                                                        \
+                name.capacity = (size);                                                                                \
+            else                                                                                                       \
+                while (name.len + (size) > name.capacity)                                                              \
+                    name.capacity *= 2;                                                                                \
+            void *ptr = realloc(name.data, sizeof(T) * name.capacity);                                                 \
+            if (ptr == NULL) {                                                                                         \
+                perror("not enough memory...");                                                                        \
+                free(name.data);                                                                                       \
+                exit(EXIT_FAILURE);                                                                                    \
+            }                                                                                                          \
+            name.data = ptr;                                                                                           \
+        }                                                                                                              \
+        memcpy(name.data + name.len, &value, sizeof(T) * (size));                                                      \
+        name.len += (size);                                                                                            \
+    } while (0);
+
+#define DA_DESTROY(name)                                                                                               \
+    do {                                                                                                               \
+        free(name.data);                                                                                               \
+        name.data = 0;                                                                                                 \
+        name.len = name.capacity = 0;                                                                                  \
+    } while (0);
+
+#define LIT2SV(lit) ((struct sv){.data = (unsigned char *)(lit), .len = sizeof(lit) - 1})
+
 struct str {
     unsigned char *data;
     size_t capacity;
@@ -19,10 +74,18 @@ struct pair {
     struct sv right;
 };
 
-
 struct str read_input();
 void str_destroy(struct str *str);
-struct pair split(struct sv sv, char byte);
+
+struct sv sv_read_input();
+
 long sv_read_int(struct sv sv);
+int sv_startswith(struct sv sv, struct sv pref);
+
+struct pair split(struct sv sv, char byte);
+struct pair sv_split_byte(struct sv sv, char byte);
+struct pair sv_split_once(struct sv sv, struct sv sep);
+
+struct sv *sv_split_array(struct sv sv, struct sv sep, size_t *n);
 
 #endif
